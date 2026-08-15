045cbef4cf2dd6f7e41d562e17e688fb20d3864c
ScarletLeaf1000 <ScarletLeaf1000@gmail.com>
2026-08-14 17:43:22 -0700

    Added block registers

--- src/main/java/me/scarletleaf1000/solarpunk/block/ModBlocks.java
+++ src/main/java/me/scarletleaf1000/solarpunk/block/ModBlocks.java
@@ -1,12 +1,29 @@
 package me.scarletleaf1000.solarpunk.block;
 
 import me.scarletleaf1000.solarpunk.Solarpunk;
+import me.scarletleaf1000.solarpunk.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(Solarpunk.MOD_ID);
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
